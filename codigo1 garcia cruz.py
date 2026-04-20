ddf9bf44fe0ecc3946bbeb438bfe1775b6f99596
cgangel02 <angelcayetanogarciacruz02@gmail.com>
2026-04-20 13:49:56 -0600

    Add files via upload

---
+++ codigo1 garcia cruz.py
@@ -0,0 +1,5 @@
+#Energia  E=mc^2
+masa= float(input("ingrese la masa en kg"))
+velocidad_luz=3e8
+energi=masa*velocidad_luz**2
+print(f"la energia equivalente es:{energia} joules")
---
+++ codigo2 GARCIA CRUZ.py
@@ -0,0 +1,5 @@
+Trabajo W=f*d
+fuerza=float(input("ingrese la fuerza en newtons (N):"))
+distancia=float(input("ingrese la distancia en metros (m):"))
+trabajo=fuerza*distancia
+print(f"el tabajo realizado es de: {trabajo}joules")
